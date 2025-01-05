e7ef4575d2f69c01920340732b2dea29d0c34ed3
mustafa-mahmoud203 <97168796+mustafa-mahmoud203@users.noreply.github.com>
2025-01-05 22:22:57 +0200

    [2381] [Time Beats: 18.80%] [Memory Beats: 21.51%] - LeetPush

--- DCP-01-25/2381-Shifting-Letters-II.cpp
+++ DCP-01-25/2381-Shifting-Letters-II.cpp
@@ -7,8 +7,7 @@ public:
             int start=vec[0], end=vec[1], direction =vec[2];
             int value=(direction == 1) ? 1 : -1;
                 pref[start]+=value;
-                pref[end+1]-=value;
-            
+                pref[end+1]-=value; 
         }
 
         for(int i=1;i<n+1;i++){
@@ -16,12 +15,15 @@ public:
         }
 
         for(int i=0;i<n;i++){
-            if(pref[i]>0){
-                s[i]='a'+ (s[i] - 'a' + pref[i])%26;
-            }
-            else if(pref[i]<0){
-                  s[i]='a'+ (s[i] - 'a' +26 + (pref[i]%26))%26;
-            }
+            // if(pref[i]>0){
+            //     s[i]='a'+ (s[i] - 'a' + pref[i])%26;
+            // }
+            // else if(pref[i]<0){
+            //       s[i]='a'+ (s[i] - 'a' +26 + (pref[i]%26))%26;
+            // }
+            int shift=(s[i] - 'a' + pref[i])%26;
+            if(shift<0)shift+=26;
+            s[i]='a'+shift;
         }
         return s;
     }
