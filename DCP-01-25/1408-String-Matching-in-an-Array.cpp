09ad19761398aa3a6e704d7d888ced567aaeba4b
mustafa-mahmoud203 <97168796+mustafa-mahmoud203@users.noreply.github.com>
2025-01-07 18:35:55 +0200

    [1408] [Time Beats: 100.00%] [Memory Beats: 37.45%] - LeetPush

--- DCP-01-25/1408-String-Matching-in-an-Array.cpp
+++ DCP-01-25/1408-String-Matching-in-an-Array.cpp
@@ -8,7 +8,7 @@ public:
 
     for(int i=0 ; i < words.size(); i++ ){
         for(int j=i+1; j < words.size();j++){
-            if(words[j].find(words[i])!= string::npos){
+            if(words[j].find(words[i])!= -1){
                 res.emplace_back(words[i]);
                 break;}
         }
